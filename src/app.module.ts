bf73256e1ab27defd08deea95732111d6219bea0
Terry Tay <terry@resynctech.com>
2021-08-06 17:31:42 +0800

    Remove unused import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/users.entity';
 
 @Module({
   imports: [
